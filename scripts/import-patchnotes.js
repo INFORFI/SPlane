8aedec33b5d75046c483f38ddb40f0f155b4dfd0
Dilgo-dev <dilgopierre@gmail.com>
2025-05-02 13:50:07 +0200

    :hammer: (scripts/import-patchnotes): Change import into mjs

--- scripts/import-patchnotes.js
+++ scripts/import-patchnotes.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
-const path = require('path');
-const { PrismaClient } = require('@prisma/client');
+import fs from 'fs';
+import path from 'path';
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
