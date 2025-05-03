8f3a9f9c9e8cd511c09e6854802d4436511c03c7
Dilgo-dev <dilgopierre@gmail.com>
2025-05-03 15:38:08 +0200

    :lipstick: (client/settings): Add settings page

---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,13 @@
+import { getUserLoggedIn } from "@/action/users/getUserLoggedIn";
+import UserSettingsPage from "./Settings";
+import { redirect } from "next/navigation";
+
+export default async function SettingsPage() {
+    const user = await getUserLoggedIn();
+
+    if (!user) {
+        redirect("/login");
+    }
+
+    return <UserSettingsPage user={user} />
+}
