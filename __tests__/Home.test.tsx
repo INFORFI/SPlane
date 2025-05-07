6ed92d1473968cb8495c28c9fc7ddab9e5ec37a7
Dilgo-dev <dilgopierre@gmail.com>
2025-05-07 10:26:59 +0200

    :white_check_mark: Add Home test

---
+++ __tests__/Home.test.tsx
@@ -0,0 +1,14 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import Home from '@/app/(public)/page';
+
+describe('Home', () => {
+    it('renders the home page', () => {
+        render(<Home />);
+
+        const title = screen.getByText('SPLANE');
+
+        expect(title).toBeInTheDocument();
+    });
+});
+
