580156f4446a395cb7a60dfdc57fd07bd1d2c3d1
Dilgo-dev <dilgopierre@gmail.com>
2025-05-07 16:20:22 +0200

    :adhesive_bandage: Fix for production

--- src/app/dashboard/patchnotes/[id]/page.tsx
+++ src/app/dashboard/patchnotes/[id]/page.tsx
@@ -1,33 +1,25 @@
 import { Suspense } from 'react';
 import { notFound } from 'next/navigation';
-import { getAllPatchnotes } from '@/action/patchnote/patchnote';
 import PatchnoteDetail from './PatchnoteDetails';
 import PatchnoteDetailSkeleton from './PatchnoteDetailSkeleton';
 import getPatchnote from '@/action/patchnote/getPatchnote';
 
-interface PatchnotePageProps {
-  params: {
-    id: string;
-  };
-}
-
-export async function generateStaticParams() {
-  const patchnotes = await getAllPatchnotes();
-  return patchnotes.map(patchnote => ({
-    id: patchnote.id.toString(),
-  }));
-}
-
-export default async function PatchnotePage({ params }: PatchnotePageProps) {
+export default async function PatchnotePage({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
+  const patchnoteId = parseInt(id);
+
+  if (isNaN(patchnoteId)) {
+    return notFound();
+  }
+
   return (
     <Suspense fallback={<PatchnoteDetailSkeleton />}>
-      <PatchnotePageContent id={id} />
+      <PatchnotePageContent id={patchnoteId} />
     </Suspense>
   );
 }
 
-async function PatchnotePageContent({ id }: { id: string }) {
+async function PatchnotePageContent({ id }: { id: number }) {
   const patchnote = await getPatchnote(id);
 
   if (!patchnote) {
